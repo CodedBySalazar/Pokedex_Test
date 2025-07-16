bbe813d5d12ee131a0a1837ff605ca211d6474da
Kendall <kendallsibaja2021@gmail.com>
2025-07-16 17:02:41 -0600

    Adding some styles to view

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Pokedex_Test
 This project consist in a pokedex where user can see the data of every pokemon selected.
+
+## How to install
+1. Clone the repository
+```
+git clone 
+```
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let actualPokemonData = [];
 
 fetchPokedexData();
 
-async function fetchPokedexData(name = "Kanto") {
+async function fetchPokedexData(name = "kanto") {
   try {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokedex/${name.toLowerCase()}`
@@ -59,6 +59,7 @@ async function fetchPokemonData(pokemonName) {
       name: pokemon.name,
       weight: pokemon.weight,
       height: pokemon.height,
+      species: specie.genera.find((g) => g.language.name === "en").genus.split(" ")[0],
       abilities: pokemon.abilities.map((a) => a.ability.name),
       types: pokemon.types.map((t) => t.type.name),
       egg_groups: specie.egg_groups.map((g) => g.name),
@@ -66,7 +67,6 @@ async function fetchPokemonData(pokemonName) {
       secondary_image: pokemon.sprites.front_default,
       evolution_names: evolution_names,
     });
-
   } catch (error) {
     console.error(`Error fetching data for ${pokemonName}:`, error.message);
   }
@@ -81,7 +81,10 @@ async function getEvolutionChain(specie) {
       const evolutionChain = await evolutionChainResponse.json();
       evolution_names = extractEvolutionNames(evolutionChain.chain);
     } catch (e) {
-      console.error(`Error fetching evolution chain for ${pokemonName}:`, e.message);
+      console.error(
+        `Error fetching evolution chain for ${pokemonName}:`,
+        e.message
+      );
     }
   }
 
@@ -111,15 +114,13 @@ function displayPokemonData(filteredPokemons = pokemonData) {
     listItem.className =
       "list-group-item list-group-item-action d-flex justify-content-between align-items-center";
     listItem.onclick = () => {
-      getPokemon(pokemon.name);
+      getSelectedPokemon(pokemon.name);
     };
 
     listItem.innerHTML = `
             <div class="d-flex justify-content-between align-items-center">
-                <img src="${pokemon.secondary_image}" alt="${
-      pokemon.name
-    }" class="img-fluid" style="max-width: 50px;">
-                <span class="fw-bold ps-2">${pokemon.name}</span>
+                <img src="${pokemon.secondary_image}" alt="${pokemon.name}" class="img-fluid" style="max-width: 50px;">
+                <span class="fw-bold ps-2 text-white">${pokemon.name}</span>
             </div>
             <div>
                 <span class="badge bg-danger rounded-pill">#${pokemon.id
@@ -132,7 +133,7 @@ function displayPokemonData(filteredPokemons = pokemonData) {
   });
 }
 
-function getPokemon(pokemonName) {
+function getSelectedPokemon(pokemonName) {
   const selectedPokemon = pokemonData.find((p) => p.name === pokemonName);
 
   if (selectedPokemon) {
@@ -147,6 +148,7 @@ function showActualPokemon() {
   const $typesContainer = $("#pokemonTypes");
   const $weight = $("#pokemonWeight");
   const $height = $("#pokemonHeight");
+  const $specie = $("#pokemonSpecie");
   const $eggGroups = $("#eggGroups");
   const $abilities = $("#abilities");
 
@@ -164,6 +166,7 @@ function showActualPokemon() {
 
   $weight.text(`${actualPokemonData.weight / 10} kg`);
   $height.text(`${actualPokemonData.height / 10} m`);
+  $specie.text(convertFirstLetterUpperCase(actualPokemonData.species));
 
   $eggGroups.empty();
   $eggGroups.text(
@@ -178,6 +181,48 @@ function showActualPokemon() {
       .map((ability) => convertFirstLetterUpperCase(ability))
       .join(", ")
   );
+
+  addImagesEvolutions();
+}
+
+function addImagesEvolutions() {
+  const evolutionsImages = [];
+
+  actualPokemonData.evolution_names.forEach((evolutionName) => {
+    const pokemon = pokemonData.find((p) => p.name === evolutionName);
+    if (pokemon) {
+      evolutionsImages.push({
+        name: convertFirstLetterUpperCase(pokemon.name),
+        image: pokemon.principal_image,
+      });
+    }
+  });
+
+    addEvolutionChainToview(evolutionsImages);
+}
+
+function addEvolutionChainToview(evolutionsImages) {
+  const $evolutionsContainer = $("#evolutionChainContainer");
+  $evolutionsContainer.empty();
+
+  evolutionsImages.forEach((evolution, index) => {
+    const isLast = index === evolutionsImages.length - 1;
+
+    const $evolutionDiv = $(`
+        <div class="col-4 col-sm-3 col-md-2 d-flex flex-column align-items-center">
+          <img src="${evolution.image}" alt="${evolution.name}" class="img-fluid">
+          <span class="mt-2 fw-bold text-center">${evolution.name}</span>
+        </div>
+      `);
+
+    $evolutionsContainer.append($evolutionDiv);
+
+    if (!isLast) {
+      $evolutionsContainer.append(`
+            <i class="bi bi-arrow-right text-danger fs-1 mx-3"></i>
+        `);
+    }
+  });
 }
 
 function convertFirstLetterUpperCase(name) {
@@ -196,4 +241,10 @@ document.getElementById("searchInput").addEventListener("input", (e) => {
   if (searchPokemon === "") {
     displayPokemonData(pokemonData);
   }
+});
+
+$("#pokemon-list").on("click", 'li', function (e) {
+    $("#pokemon-list li").removeClass("active-pokemon");
+
+    $(this).addClass("active-pokemon");
 });
